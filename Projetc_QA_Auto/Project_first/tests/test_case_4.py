b58f877942cf6111dc0f4b76b9d74f2c3575f100
boris <boristondjua@gmail.com>
2026-03-19 15:47:17 +0100

    Ther test number 4 ist passing on chrome and firefox

---
+++ Projetc_QA_Auto/Project_first/tests/test_case_4.py
@@ -0,0 +1,36 @@
+from playwright.sync_api import Page, expect, Playwright
+
+import time
+
+
+def test_Logout_User(page: Page):
+    page.goto("https://www.automationexercise.com/")
+    expect(page.get_by_text("Video Tutorials")).to_be_visible()
+    page.get_by_role("button", name="Einwilligen").click()
+    page.get_by_role("link", name="Signup / Login").click()
+    expect(page.get_by_text("Login to your account")).to_be_visible()
+    page.locator('[data-qa="login-email"]').fill("freedomvision@gmail.com")
+    page.locator('[data-qa="login-password"]').fill("Freedom95")
+    page.locator('[data-qa="login-button"]').click()
+    expect(page.get_by_text("Logged in as 09w0823@Freedom")).to_be_visible()
+    page.get_by_role("link", name="logout").click()
+    expect(page.get_by_text("Login to your account")).to_be_visible()
+    time.sleep(4)
+
+
+# firefox
+def test_Logout_User_firefox(playwright: Playwright):
+    firefoxBrowser = playwright.firefox.launch(headless=False)
+    page = firefoxBrowser.new_page()
+    page.goto("https://www.automationexercise.com/")
+    expect(page.get_by_text("Video Tutorials")).to_be_visible()
+    page.get_by_role("button", name="consent").click()
+    page.get_by_role("link", name="Signup / Login").click()
+    expect(page.get_by_text("Login to your account")).to_be_visible()
+    page.locator('[data-qa="login-email"]').fill("freedomvision@gmail.com")
+    page.locator('[data-qa="login-password"]').fill("Freedom95")
+    page.locator('[data-qa="login-button"]').click()
+    expect(page.get_by_text("Logged in as 09w0823@Freedom")).to_be_visible()
+    page.get_by_role("link", name="logout").click()
+    expect(page.get_by_text("Login to your account")).to_be_visible()
+    time.sleep(4)
