b86d6084e9f13713db907f719a49311eb581a287
boris <boristondjua@gmail.com>
2026-03-19 16:12:47 +0100

    Test Case 5: Register User with existing email successful finished on chrome and firefox

---
+++ Projetc_QA_Auto/Project_first/tests/test_case_5.py
@@ -0,0 +1,34 @@
+from playwright.sync_api import Page, expect, Playwright
+
+import time
+
+
+# Test Case 5: Register User with existing email
+def test_Register_User_with_existing_emai(page: Page):
+    page.goto("https://www.automationexercise.com/")
+    expect(page.get_by_text("Video Tutorials")).to_be_visible()
+    page.get_by_role("button", name="Einwilligen").click()
+    page.get_by_role("link", name="Signup / Login").click()
+    expect (page.get_by_text("New User Signup!")).to_be_visible()
+    page.locator('[data-qa="signup-name"]').fill("09w0823@Freedom")
+    page.locator('[data-qa="signup-email"]').fill("freedomvision@gmail.com")
+    page.locator('[data-qa="signup-button"]').click()
+    expect(page.get_by_text("Email Address already exist")).to_be_visible()
+    time.sleep(4)
+
+
+# firefox
+def test_Register_User_with_existing_emai_firefox(playwright: Playwright):
+    firefoxBrowser = playwright.firefox.launch(headless=False)
+    page = firefoxBrowser.new_page()
+    page.goto("https://www.automationexercise.com/")
+    expect(page.get_by_text("Video Tutorials")).to_be_visible()
+    page.get_by_role("button", name="consent").click()
+    page.get_by_role("link", name="Signup / Login").click()
+    expect (page.get_by_text("New User Signup!")).to_be_visible()
+    page.locator('[data-qa="signup-name"]').fill("09w0823@Freedom")
+    page.locator('[data-qa="signup-email"]').fill("freedomvision@gmail.com")
+    page.locator('[data-qa="signup-button"]').click()
+    expect(page.get_by_text("Email Address already exist")).to_be_visible()
+    firefoxBrowser.close()
+    time.sleep(4)
